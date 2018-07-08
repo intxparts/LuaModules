22d4af2b51912d765f319ec97b95f84f92f0159f
intxparts <intxparts@gmail.com>
2018-07-08 11:43:57 -0400

    added str.join and str.rep util functions.

--- src/str.lua
+++ src/str.lua
@@ -22,4 +22,21 @@ function str.to_integer(s)
     return math.floor(number)
 end
 
+function str.rep(s, n, sep)
+    assert(type(s) == 'string')
+    assert(type(n) == 'number')
+    local str_table = {}
+    local i = 0
+    while i < n do
+        table.insert(str_table, s)
+        i = i + 1
+    end
+    local result = table.concat(str_table, sep)
+    return result
+end
+
+function str.join(...)
+    return table.concat(arg)
+end
+
 return str
