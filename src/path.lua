b07f74352aac488b3522ed12c3f968c799f76353
intxparts <intxparts@gmail.com>
2018-07-08 18:00:27 -0400

    added linux commands for listdir and listfiles

--- src/path.lua
+++ src/path.lua
@@ -25,6 +25,15 @@ local function _wincmd_listfiles(root_directory, include_subdirectories)
     return command
 end
 
+local function _unixcmd_listfiles(root_directory, include_subdirectories)
+    local depth = ''
+    if not include_subdirectories then
+        depth = '-maxdepth 1'
+    end
+    local command = string.format('find "%s" "%s" -type f', root_directory, depth)
+    return command
+end
+
 -- returns windows specific cmd string to list all directories in the root_directory
 -- with the option to also include subdirectories
 local function _wincmd_listdir(root_directory, include_subdirectories)
@@ -34,6 +43,15 @@ local function _wincmd_listdir(root_directory, include_subdirectories)
     if include_subdirectories then
         command = command .. ' /s'
     end
+    return command
+end
+
+local function _unixcmd_listdir(root_directory, include_subdirectories)
+    local depth = ''
+    if not include_subdirectories then
+        depth = '-maxdepth 1'
+    end
+    local command = string.format('find "%s" "%s" -type d', root_directory, depth)
     return command
 end
 
@@ -53,11 +71,12 @@ function path.exists(target_path)
     assert(type(target_path) == 'string')
     local result = nil
     if os_ext.is_windows then
-        result = os_ext.run_command(string.format('if exist "%s" (echo true) else (echo false)', target_path)) 
+        result = os_ext.run_command(string.format('if exist "%s" (echo true) else (echo false)', target_path))[1] == 'true'
     else
-        result = os_ext.run_command(string.format('if test "%s" then echo true; else echo false; fi', target_path))
+        result = os_ext.run_command(string.format('if test -f "%s" then; echo true; else echo false; fi', target_path))[1] == 'true' or
+            os_ext.run_command(string.format('if test -d "%s" then; echo true; else echo false; fi', target_path))[1] == 'true'
     end
-    return result[1] == 'true'
+    return result
 end
 
 function path.list_files(root_directory, include_subdirectories)
@@ -68,8 +87,7 @@ function path.list_files(root_directory, include_subdirectories)
     if os_ext.is_windows then
         command = _wincmd_listfiles(root_directory, include_subdirectories)
     else
-        -- TODO: add linux command
-        assert(true == false)
+        command = _unixcmd_listfiles(root_directory, include_subdirectories)
     end
     return os_ext.run_command(command)
 end
@@ -82,8 +100,7 @@ function path.list_dir(root_directory, include_subdirectories)
     if os_ext.is_windows then
         command = _wincmd_listdir(root_directory, include_subdirectories)
     else
-        -- TODO: add linux command
-        assert(true == false)
+        command = _unixcmd_listdir(root_directory, include_subdirectories)
     end
     return os_ext.run_command(command)
 end
