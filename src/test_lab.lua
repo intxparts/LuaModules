66c40cb72ce7305eeef7fdb87e48c4f4425b5c96
intxparts <jcowens.dx@gmail.com>
2019-06-22 17:30:10 -0400

    clean: remove old comments

--- src/test_lab.lua
+++ src/test_lab.lua
@@ -37,9 +37,6 @@ test_lab._current = {
     tests = {} -- support for tests outside of groups
 }
 
--- [] need to clean up what is print out to console & make it more readable
--- [] currently adding print statements to unit tests that are failing do not work - only on ones that succeed - need to fix this
-
 function test_lab:group(description, fn, tags)
     assert(type(fn) == 'function')
     local child = {
