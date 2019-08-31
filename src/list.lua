fa2e6eba15807b9b267131bd18a56305f62a4e6e
intxparts <jcowens.dx@gmail.com>
2019-08-31 16:15:18 -0400

    clean: renamed array->list

---
+++ src/list.lua
@@ -0,0 +1,179 @@
+local list = {}
+
+
+local err_idx_out_bounds = 'list index out of bounds'
+local err_idx_invalid = 'index invalid, must be an int'
+
+
+function list.__index(t, k)
+--	print('indexing table with key', k)
+	if type(k) == 'number' then
+		assert(math.floor(k) == k, err_idx_invalid)
+		assert(0 < k and k <= t._len, err_idx_out_bounds)
+		return rawget(t, k)
+	else
+		return list[k]
+	end
+end
+
+
+function list.__newindex(t, k, v)
+--	print('new index table with key, value', k, v)
+	assert(false, '__newindex not supported, use insert* functions')
+end
+
+
+function list:length()
+	return self._len
+end
+
+
+function list.new(t)
+	local _t = t or {}
+	local a = { _len = #_t }
+	for i=1, a._len do
+		rawset(a, i, t[i])
+	end
+	setmetatable(a, list)
+	return a
+end
+
+
+function list:insert(e)
+	self._len = self._len + 1
+	rawset(self, self._len, e)
+end
+
+
+function list:insert_at(e, i)
+	assert(1 <= i and i <= self._len, err_idx_out_bounds)
+	for j = self._len, i, -1 do
+		rawset(self, j+1, rawget(self, j))
+	end
+	rawset(self, i, e)
+	self._len = self._len + 1
+end
+
+
+function list:insert_range_at(arr, idx)
+	assert(getmetatable(arr) == list)
+	assert(1 <= idx and idx <= self._len, err_idx_out_bounds)
+	for j = idx, self._len do
+		local new_idx = idx + arr._len
+		rawset(self, new_idx, rawget(self, j))
+	end
+
+	for j = 1, arr._len do
+		local new_idx = idx + j - 1
+		rawset(self, new_idx, rawget(arr, j))
+	end
+	self._len = self._len + count
+end
+
+
+function list:index_of(e, start_idx, end_idx)
+	local s_idx = start_idx or 1
+	local e_idx = end_idx or self._len
+	assert(1 <= s_idx and s_idx <= e_idx)
+	assert(s_idx <= e_idx and e_idx <= self._len)
+
+	for j = s_idx, e_idx do
+		if rawget(self, j) == e then
+			return j
+		end
+	end
+	return -1
+end
+
+
+function list:contains(e)
+	local idx = self:index_of(e, 1, self._len)
+	return idx ~= -1
+end
+
+
+function list:last_index_of(e, start_idx, end_idx)
+	local s_idx = start_idx or 1
+	local e_idx = end_idx or self._len
+	assert(1 <= s_idx and s_idx <= e_idx)
+	assert(s_idx <= e_idx and e_idx <= self._len)
+
+	for j = e_idx, s_idx, -1 do
+		if rawget(self, j) == e then
+			return j
+		end
+	end
+	return -1
+end
+
+
+function list:clone()
+	local a = list.new()
+	for j = 1, self._len do
+		rawset(a, j, rawget(self, j))
+	end
+	a._len = self._len
+	return a
+end
+
+
+function list:remove_at(i)
+	assert(1 <= i and i <= self._len)
+	for j = i, self._len do
+		rawset(self, j, rawget(self, j+1))
+	end
+	self._len = self._len - 1
+end
+
+
+function list:remove(e)
+	local found = false
+	for j = 1, self._len do
+		if not found and rawget(self, j) == e then
+			found = true
+		end
+
+		if found then
+			rawset(self, j, rawget(self, j+1))
+		end
+	end
+	if found then
+		self._len = self._len - 1
+	end
+end
+
+
+function list:remove_range_at(start_idx, remove_count)
+	local s_idx = start_idx
+	local e_idx = s_idx + remove_count
+	assert(1 <= s_idx and s_idx <= e_idx)
+	assert(s_idx <= e_idx and e_idx <= self._len)
+	
+	for i = s_idx, self._len do
+		rawset(self, i, rawget(self, i + remove_count))
+	end
+
+	self._len = self._len - remove_count
+end
+
+
+function list:clear()
+	for j = 1, self._len do
+		rawset(self, j, nil)
+	end
+	self._len = 0
+end
+
+
+function list:reverse()
+	local half_len = self._len / 2
+	for j = 1, half_len do
+		local swap_idx = self._len + 1 - j
+		local tmp = rawget(self, j)
+		rawset(self, j, rawget(self, swap_idx))
+		rawset(self, swap_idx, tmp)
+	end
+end
+
+
+return list
---
+++ tests/list_tests.lua
@@ -0,0 +1,333 @@
+package.path = package.path .. '; ..\\?.lua; ..\\src\\?.lua'
+
+local tl = require('test_lab')
+local list = require('list')
+
+tl:group('list.new() ->', function()
+	tl:test('initializes an empty list with _len == 0', function()
+		local a = list.new()
+		assert(a._len == 0)
+		local result, err = pcall(function() return a[1] end)
+		assert(not result)
+	end)
+end)
+
+tl:group('list:insert(e [any]) ->', function()
+	tl:test('inserts an element e to the end of the list and increments _len by 1', function()
+		local a = list.new()
+		a:insert(1)
+		a:insert(3)
+		assert(a[1] == 1 and a[2] == 3)
+		assert(a._len == 2)
+	end)
+
+	tl:test('no arg inserts nil', function()
+		local a = list.new()
+		a:insert()
+		assert(a._len == 1)
+		assert(a[1] == nil)
+	end)
+
+	tl:test('nil arg inserts nil', function()
+		local a = list.new()
+		a:insert(nil)
+		assert(a._len == 1)
+		assert(a[1] == nil)
+	end)
+
+	tl:test('insert table element', function()
+		local a = list.new()
+		local e = {age=22, name='john doe'}
+		a:insert(e)
+		assert(a[1] == e)
+		assert(a._len == 1)
+	end)
+
+	tl:test('multiple inserts', function()
+		local a = list.new()
+		for i=1, 100 do
+			a:insert(i)
+		end
+		for i=1, 100 do
+			assert(a[i] == i)
+		end
+		assert(a._len == 100)
+	end)
+
+end)
+
+tl:group('list:insert_at(e [any], i [int]) ->', function()
+	tl:test('inserts an element e at index i in the list and increases _len by 1', function()
+		local a = list.new()
+		a:insert(4)
+		a:insert(1)
+		a:insert_at(2, 2)
+		assert(a[1] == 4 and a[2] == 2 and a[3] == 1)
+		assert(a._len == 3)
+	end)
+
+	tl:test('no args raises error', function()
+		local a = list.new()
+		local success, err = pcall(function() a:insert_at() end)
+		assert(not success)
+	end)
+
+	tl:test('single arg raises error', function()
+		local a = list.new()
+		local success, err = pcall(function() a:insert_at(1) end)
+		assert(not success)
+	end)
+
+	tl:test('non-number idx raises error', function()
+		local a = list.new()
+		local success, err = pcall(function() a:insert_at({}) end)
+		assert(not success)
+	end)
+
+	tl:test('0 idx raises index out of range error', function()
+		local a = list.new()
+		a:insert(-1.2)
+		local success, err = pcall(function() a:insert_at(2.3, 0) end)
+		assert(not success and err:find('list index out of bounds') ~= nil)
+	end)
+
+	tl:test('inst._len + 1 idx raises index out of range error', function()
+		local a = list.new()
+		a:insert({name='john'})
+		local idx = a._len + 1
+		local success, err = pcall(function() a:insert_at({name='jane'}, idx) end)
+		assert(not success and err:find('list index out of bounds') ~= nil)
+	end)
+
+end)
+
+tl:group('list:length() ->', function()
+	tl:test('returns the number of elements in the list, tracked by inst._len', function()
+		local a = list.new()
+		a:insert(1)
+		a:insert(2)
+		assert(a._len == 2)
+		assert(a:length() == 2)
+	end)
+
+	tl:test('empty list returns 0', function()
+		local a = list.new()
+		assert(a._len == 0)
+		assert(a:length() == 0)
+	end)
+
+	tl:test('changes to a._len are observed', function()
+		local a = list.new()
+		a._len = 10
+		assert(a:length() == 10)
+	end)
+
+end)
+
+tl:group('list:clear() ->', function()
+	tl:test('removes all elements and sets inst._len = 0', function()
+		local a = list.new()
+		a:insert(1)
+		a:insert(2)
+		a:insert(3)
+		assert(a._len == 3)
+		a:clear()
+		assert(a._len == 0)
+		local result, err = pcall(function() return a[1] end)
+		assert(not result)
+	end)
+end)
+
+tl:group('list:remove(e [any]) ->', function()
+	tl:test('removes the target element [number] and reduces inst._len by 1', function()
+		local a = list.new()
+		a:insert(1)
+		a:insert(2)
+		assert(a._len == 2)
+		a:remove(2)
+		assert(a._len == 1 and a[1] == 1)
+	end)
+
+	tl:test('removes the target element [string] and reduces inst._len by 1', function()
+		local a = list.new()
+		a:insert('hello')
+		a:insert('world')
+		assert(a._len == 2)
+		a:remove('world')
+		assert(a._len == 1 and a[1] == 'hello')
+	end)
+
+	tl:test('removes the target element [table] and reduces inst._len by 1', function()
+		local a = list.new()
+		local pt1 = {x=0}
+		local pt2 = {x=2}
+		a:insert(pt1)
+		a:insert(pt2)
+		assert(a._len == 2)
+		a:remove(pt1)
+		assert(a._len == 1 and a[1] == pt2)
+	end)
+
+	tl:test('removes the target element [bool] and reduces inst._len by 1', function()
+		local a = list.new()
+		a:insert(true)
+		a:insert(false)
+		assert(a._len == 2)
+		a:remove(false)
+		assert(a._len == 1 and a[1] == true)
+	end)
+
+	tl:test('removes the first element [number] found in list when there are multiple', function()
+		local a = list.new()
+		a:insert(1)
+		a:insert(2)
+		a:insert(1)
+		assert(a._len == 3)
+		a:remove(1)
+		assert(a._len == 2 and a[1] == 2 and a[2] == 1)
+	end)
+
+	tl:test('removes the first element [table] found in list when there are multiple', function()
+		local a = list.new()
+		local pt1 = { x=0, y=0 }
+		local pt2 = { x=1, y=0 }
+		a:insert(pt1)
+		a:insert(pt2)
+		a:insert(pt1)
+		assert(a._len == 3)
+		a:remove(pt1)
+		assert(a._len == 2 and a[1] == pt2 and a[2] == pt1)
+	end)
+
+	tl:test('removes the first element [string] found in list when there are multiple', function()
+		local a = list.new()
+		a:insert('hello')
+		a:insert('world')
+		a:insert('hello')
+
+		assert(a._len == 3)
+		a:remove('hello')
+		assert(a._len == 2 and a[1] == 'world' and a[2] == 'hello')
+	end)
+
+	tl:test('removes the first element [bool] found in list when there are multiple', function()
+		local a = list.new()
+		a:insert(true)
+		a:insert(false)
+		a:insert(true)
+
+		assert(a._len == 3)
+		a:remove(false)
+		assert(a._len == 2 and a[1] == true and a[2] == true)
+	end)
+
+	tl:test('does nothing when target element [int] is not in the list', function()
+		local a = list.new()
+		a:insert(1)
+		a:insert(2)
+
+		a:remove(3)
+		assert(a._len == 2 and a[1] == 1 and a[2] == 2)
+	end)
+
+	tl:test('does nothing when target element [table] is not in the list', function()
+		local a = list.new()
+		local pt1 = {x=0}
+		local pt2 = {x=1}
+		a:insert(pt1)
+		a:insert(pt2)
+
+		a:remove({x=0})
+		assert(a._len == 2 and a[1] == pt1 and a[2] == pt2)
+	end)
+
+end)
+
+tl:group('list.__index ->', function()
+	tl:test('returns the value stored at index k [int]', function()
+		local a = list.new()
+		a:insert('hello')
+		a:insert('aloha')
+		a:insert('goodbye')
+
+		assert(a[1] == 'hello' and a[2] == 'aloha' and a[3] == 'goodbye')
+	end)
+
+	tl:test('raises an error for index k, k == 0; list index out of bounds', function()
+		local a = list.new()
+		a:insert(1)
+		local result, err = pcall(function() return a[0] end)
+		assert(not result and err:find('list index out of bounds') ~= nil)
+	end)
+
+	tl:test('raises an error for index k, k < 0; list index out of bounds', function()
+		local a = list.new()
+		a:insert(1)
+		local result, err = pcall(function() return a[-1] end)
+		assert(not result and err:find('list index out of bounds') ~= nil)
+	end)
+
+	tl:test('raises an error for index k, k > inst._len; list index out of bounds', function()
+		local a = list.new()
+		a:insert(2)
+		local result, err = pcall(function() return a[a._len + 1] end)
+		assert(not result and err:find('list index out of bounds') ~= nil)
+	end)
+end)
+
+tl:group('list.__newindex ->', function()
+	tl:test('raises an error for attempting to add properties onto the list', function()
+		local a = list.new()
+		local result, err = pcall(function() a.greet = 'hello' end)
+		assert(not result)
+	end)
+
+	tl:test('raises an error for attempting to add an element at 0 index', function()
+		local a = list.new()
+		local result, err = pcall(function() a[0] = 2 end)
+		assert(not result)
+	end)
+
+	tl:test('raises an error for attempting to add an element at -1 index', function()
+		local a = list.new()
+		local result, err = pcall(function() a[-1] = 2 end)
+		assert(not result)
+	end)
+
+end)
+
+tl:group('lua standard library compatibility ->', function()
+	tl:test('# can be used to determine the length', function()
+		local a = list.new()
+		assert(#a == 0)
+		assert(a._len == 0)
+		a:insert(1)
+		a:insert(2)
+		assert(#a == 2)
+		assert(a._len == 2)
+	end)
+end)
+
+if _VERSION == 'Lua 5.3' then
+tl:group('Lua 5.3 ->', function()
+	tl:test('unable to iterate with ipairs, since ipairs indexes tables regardless of out of bounds', function()
+		local a = list.new()
+		a:insert(1)
+		a:insert(2)
+		local result, err = pcall(function() for i, v in ipairs(a) do local t = v end end)
+		assert(not result)
+	end)
+end)
+elseif _VERSION == 'Lua 5.1' then
+tl:group('Lua 5.1 (Luajit) ->', function()
+	tl:test('is able to iterate with ipairs', function()
+		local a = list.new()
+		a:insert(1)
+		a:insert(2)
+		for i, v in ipairs(a) do
+			a[i] = v + 1
+		end
+		assert(a[1] == 2 and a[2] == 3)
+	end)
+end)
+end
