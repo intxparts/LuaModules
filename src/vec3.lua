60958168085457cca95d0583281334984054a684
intxparts <jcowens.dx@gmail.com>
2020-01-01 06:25:07 -0500

    clean: remove unecessary functions. switch back from int indexing to x,y,z

--- src/vec3.lua
+++ src/vec3.lua
@@ -2,7 +2,7 @@ local vec3 = {}
 vec3.__index = vec3
 
 function vec3.new(x, y, z)
-    local v = { [1]=x or 0, [2]=y or 0, [3]=z or 0}
+    local v = { x=x or 0, y=y or 0, z=z or 0}
     setmetatable(v, vec3)
     return v
 end
@@ -13,14 +13,14 @@ end
 
 function vec3.from_points(p1, p2)
     return vec3.new(
-        p2[1] - p1[1],
-        p2[2] - p1[2],
-        p2[3] - p1[3]
+        p2.x - p1.x,
+        p2.y - p1.y,
+        p2.z - p1.z
     )
 end
 
 function vec3.dot(v1, v2)
-    return v1[1]*v2[1] + v1[2]*v2[2] + v1[3]*v2[3]
+    return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z
 end
 
 function vec3.mag2(v)
@@ -31,109 +31,70 @@ function vec3.mag(v)
     return math.sqrt(vec3.mag2(v))
 end
 
+function vec3.scale(v, k)
+	return vec3.new(v.x*k, v.y*k, v.z*k)
+end
+
 function vec3.cross(v1, v2)
     return vec3.new(
-        v1[2]*v2[3] - v1[3]*v2[2],
-        v1[3]*v2[1] - v1[1]*v2[3],
-        v1[1]*v2[2] - v1[2]*v2[1]
+        v1.y*v2.z - v1.z*v2.y,
+        v1.z*v2.x - v1.x*v2.z,
+        v1.x*v2.y - v1.y*v2.x
     )
 end
 
-function vec3.__add(v1, v2)
+function vec3.add(v1, v2)
     return vec3.new(
-        v1[1] + v2[1],
-        v1[2] + v2[2],
-        v1[3] + v2[3]
+        v1.x + v2.x,
+        v1.y + v2.y,
+        v1.z + v2.z
     )
 end
 
-function vec3.__sub(v1, v2)
+function vec3.sub(v1, v2)
     return vec3.new(
-        v1[1] - v2[1],
-        v1[2] - v2[2],
-        v1[3] - v2[3]
+        v1.x - v2.x,
+        v1.y - v2.y,
+        v1.z - v2.z
     )
 end
 
-function vec3.__mul(l, r)
-	local v = nil
-	local k = nil
-	
-	if type(l) == 'number' then
-		k = l
-		v = r
-	else
-		k = r
-		v = l
-	end
-
-    return vec3.new(
-        v[1]*k,
-        v[2]*k,
-        v[3]*k
-    )
+function vec3.equals(v1, v2)
+	return v1.x == v2.x and v1.y == v2.y and v1.z == v2.z
 end
 
-function vec3.__eq(v1, v2)
-	for i = 1, 3 do
-		if v1[i] ~= v2[i] then
-			return false
-		end
-	end
-	return true
+function vec3.tostring(v)
+    return string.format('<%d, %d, %d>', v.x, v.y, v.z)
 end
-
-function vec3.__tostring(v)
-    return string.format('<%d, %d, %d>', v[1], v[2], v[3])
-end
-
-vec3.equals = vec3.__eq
-vec3.tostring = vec3.__tostring
-
---
--- Instance dependent functions
---
 
 -- v1 = v1 + v2
-function vec3:_add(v)
-    self[1] = self[1] + v[1]
-    self[2] = self[2] + v[2]
-    self[3] = self[3] + v[3]
-    return self
+function vec3._add(v1, v2)
+    v1.x = v1.x + v2.x
+	v1.y = v1.y + v2.y
+	v1.z = v1.z + v2.z
+	return v1
 end
 
 -- v1 = v1 - v2 
-function vec3:_sub(v)
-    self[1] = self[1] - v[1]
-    self[2] = self[2] - v[2]
-    self[3] = self[3] - v[3]
-    return self
+function vec3._sub(v1, v2)
+	v1.x = v1.x - v2.x
+	v1.y = v1.y - v2.y
+	v1.z = v1.z - v2.z
+    return v1
 end
 
 -- v1 = v1 * k
-function vec3:_scale(k)
-    self[1] = self[1] * k
-    self[2] = self[2] * k
-    self[3] = self[3] * k
-    return self
+function vec3._scale(v, k)
+    v.x = v.x * k
+    v.y = v.y * k
+    v.z = v.z * k
+    return v
 end
 
-function vec3:set(x, y, z)
-    self[1] = x
-    self[2] = y
-    self[3] = z
-end
-
-function vec3:_mag()
-    return vec3.mag(self)
-end
-
-function vec3:_dot(v)
-    return vec3.dot(self, v)
-end
-
-function vec3:_cross(v)
-    return vec3.cross(self, v)
+function vec3.set(v, x, y, z)
+    v.x = x
+    v.y = y
+    v.z = z
 end
 
 return vec3
--- tests/vec3_tests.lua
+++ tests/vec3_tests.lua
@@ -4,57 +4,53 @@ local test_lab = require('test_lab')
 local vec3 = require('vec3')
 
 test_lab:group('vec3.new(x [number], y [number], z [number]) -> [vec3]', function()
-    test_lab:test('create new vec3', function()
+    test_lab:test('vec3.new(0,1,0) creates a vec3 at (0,1,0)', function()
         local v1 = vec3.new(0, 1, 0)
-        assert(v1[1] == 0 and v1[2] == 1 and v1[3] == 0)
+        assert(v1.x == 0 and v1.y == 1 and v1.z == 0)
     end)
 
-    -- ensure the default vec3 is the origin (0,0,0)
-    test_lab:test('create new vec3 with no args', function()
+    test_lab:test('vec3.new() creates a vec3 at the origin (0,0,0)', function()
         local v1 = vec3.new()
-
-        assert(v1[1] == 0 and v1[2] == 0 and v1[3] == 0)
+        assert(v1.x == 0 and v1.y == 0 and v1.z == 0)
     end)
 
-    -- ensure each new vec3 is a different instance
-    test_lab:test('create multiple vec3', function()
+    test_lab:test('each call to vec3.new() creates a new instance', function()
         local v1 = vec3.new(0, 1, 0)
-        local v2 = vec3.new(0, 0, 1)
-        assert(v1[1] == 0 and v1[2] == 1 and v1[3] == 0)
-        assert(v2[1] == 0 and v2[2] == 0 and v2[3] == 1)
+        local v2 = vec3.new(0, 1, 0)
+		assert(v1 ~= v2)
+        assert(v1.x == 0 and v1.y == 1 and v1.z == 0)
+        assert(v2.x == 0 and v2.y == 1 and v2.z == 0)
     end)
 end)
 
-test_lab:group('vec3.zero() -> [vec3<0,0,0>]', function()
-    test_lab:test('== <0, 0, 0>', function()
+test_lab:group('vec3.zero() -> [vec3]', function()
+    test_lab:test('vec3.zero() == <0, 0, 0>', function()
         local v1 = vec3.zero()
-        assert(v1[1] == 0 and v1[2] == 0 and v1[3] == 0)
+        assert(v1.x == 0 and v1.y == 0 and v1.z == 0)
     end)
 end)
 
-test_lab:group('vec3.from_points(p1 [table], p2 [table]) -> [vec3<p2[1] - p1[1], p2[2] - p1[2], p2[3] - p1[3]>]', function()
+test_lab:group('vec3.from_points(p1 [table], p2 [table]) -> [vec3]', function()
     test_lab:test('create new vec3', function()
-        local p1 = { 0, 1, 1 }
-        local p2 = { 1, 0, 0 }
+        local p1 = { x=0, y=1, z=1 }
+        local p2 = { x=1, y=0, z=0 }
         local v1 = vec3.from_points(p1, p2)
-        assert(v1[1] == 1 and v1[2] == -1 and v1[3] == -1)
+        assert(v1.x == 1 and v1.y == -1 and v1.z == -1)
     end)
 end)
 
-test_lab:group('vec3.dot(v1 [table], v2 [table] -> [number<v1[1]*v2[1] + v1[2]*v2[2] + v1[3]*v2[3]>]', function()
-    test_lab:test('sum of multiplied components', function()
+test_lab:group('vec3.dot(v1 [table], v2 [table] -> [number]', function()
+    test_lab:test('vec3.dot(<1,-2,1>, <0,3,4>) == -2', function()
         local v1 = vec3.new(1, -2, 1)
         local v2 = vec3.new(0, 3, 4)
         assert(vec3.dot(v1, v2) == -2)
     end)
-end)
 
-test_lab:group('vec3:dot(v [table]) -> [number<self[1]*v[1] + self[2]*v[2] + self[3]*v[3]>]', function()
-    test_lab:test('sum of multiplied components', function()
-        local v1 = vec3.new(1, -2, 1)
-        local v2 = vec3.new(0, 3, 4)
-        assert(v1:_dot(v2) == -2)
-    end)
+	test_lab:test('v1:dot(v2)', function()
+		local v1 = vec3.new(1,2,3)
+		local v2 = vec3.new(1,1,0)
+		assert(v1:dot(v2) == 3)
+	end)
 end)
 
 test_lab:group('vec3.cross(v1 [table], v2 [table]) -> [vec3]', function()
@@ -62,28 +58,28 @@ test_lab:group('vec3.cross(v1 [table], v2 [table]) -> [vec3]', function()
         local v1 = vec3.new(1, 0, 0)
         local v2 = vec3.new(0, 1, 0)
         local actual = vec3.cross(v1, v2)
-        assert(vec3.new(0, 0, 1) == actual)
+        assert(vec3.new(0, 0, 1):equals(actual))
     end)
 
     test_lab:test('<0, 1, 0> X <1, 0, 0> = <0, 0, -1>', function()
         local v1 = vec3.new(0, 1, 0)
         local v2 = vec3.new(1, 0, 0)
         local actual = vec3.cross(v1, v2)
-        assert(vec3.new(0, 0, -1) == actual)
+        assert(vec3.new(0, 0, -1):equals(actual))
     end)
 
     test_lab:test('<1, 1, 1> X zero() = zero()', function()
         local v1 = vec3.new(1, 1, 1)
         local v2 = vec3.zero()
         local actual = vec3.cross(v1, v2)
-        assert(v2 == actual)
+        assert(v2:equals(actual))
     end)
 
     test_lab:test('parallel vec3 crossed = zero()', function()
         local v1 = vec3.new(1, 1, 4)
         local v2 = vec3.new(2, 2, 8)
         local actual = vec3.cross(v1, v2)
-        assert(actual == vec3.zero())
+        assert(actual:equals(vec3.zero()))
     end)
 end)
 
@@ -91,36 +87,33 @@ test_lab:group('vec3:cross(v [table]) -> [vec3]', function()
     test_lab:test('<1, 0, 0> X <0, 1, 0> = <0, 0, 1>', function()
         local v1 = vec3.new(1, 0, 0)
         local v2 = vec3.new(0, 1, 0)
-        local actual = v1:_cross(v2)
-        assert(vec3.new(0, 0, 1) == actual)
-
-        -- v:_cross is not a self instance modifying function
-        assert(v1 ~= actual)
+        local actual = v1:cross(v2)
+        assert(vec3.new(0, 0, 1):equals(actual))
     end)
 
     test_lab:test('<0, 1, 0> X <1, 0, 0> = <0, 0, -1>', function()
         local v1 = vec3.new(0, 1, 0)
         local v2 = vec3.new(1, 0, 0)
-        local actual = v1:_cross(v2)
-        assert(vec3.new(0, 0, -1) == actual)
+        local actual = v1:cross(v2)
+        assert(vec3.new(0, 0, -1):equals(actual))
     end)
 
     test_lab:test('<1, 1, 1> X zero() = zero()', function()
         local v1 = vec3.new(1, 1, 1)
         local v2 = vec3.zero()
-        local actual = v1:_cross(v2)
-        assert(v2 == actual)
+        local actual = v1:cross(v2)
+        assert(v2:equals(actual))
     end)
 
     test_lab:test('parallel vec3 crossed = zero()', function()
         local v1 = vec3.new(1, 1, 4)
         local v2 = vec3.new(2, 2, 8)
-        local actual = v1:_cross(v2)
-        assert(actual == vec3.zero())
+        local actual = v1:cross(v2)
+        assert(actual:equals(vec3.zero()))
     end)
 end)
 
-test_lab:group('vec3.mag(v [table]) -> [number<math.sqrt(v[1]*v[1] + v[2]*v[2] + v[3]*v[3])>]', function()
+test_lab:group('vec3.mag(v [table]) -> [number]', function()
     test_lab:test('mag(zero())', function()
         local v1 = vec3.zero()
         assert(vec3.mag(v1) == 0)
@@ -131,13 +124,13 @@ test_lab:group('vec3.mag(v [table]) -> [number<math.sqrt(v[1]*v[1] + v[2]*v[2] +
         assert(vec3.mag(v1) == 1)
     end)
 
-    test_lab:test('mag(<3, 4, 5>)', function()
+    test_lab:test('mag(3, 4, 5)', function()
         local v1 = vec3.new(3, 4, 5)
         assert(vec3.mag(v1) == math.sqrt(50))
     end)
 end)
 
-test_lab:group('vec3.mag2(v [table]) -> [number<v[1]*v[1] + v[2]*v[2] + v[3]*v[3]>]', function()
+test_lab:group('vec3.mag2(v [table]) -> [number]', function()
     test_lab:test('mag2(zero())', function()
         local v1 = vec3.zero()
         assert(vec3.mag2(v1) == 0)
@@ -148,217 +141,114 @@ test_lab:group('vec3.mag2(v [table]) -> [number<v[1]*v[1] + v[2]*v[2] + v[3]*v[3
         assert(vec3.mag2(v1) == 1)
     end)
 
-    test_lab:test('mag2(<3, 4, 5>)', function()
+    test_lab:test('mag2(3, 4, 5)', function()
         local v1 = vec3.new(3, 4, 5)
         assert(vec3.mag2(v1) == 50)
     end)
 end)
 
-test_lab:group('vec3.__eq(v1 [table], v2 [table]) -> [boolean]', function()
+test_lab:group('vec3.equals(v1 [table], v2 [table]) -> [boolean]', function()
     test_lab:test('compare same vec3', function()
         local v1 = vec3.new(1, 0, 0)
         local v2 = v1
-        assert(v1 == v2)
-        -- remove the vec3 metatable to compare that the tables are indeed the same instances of vec3
-        setmetatable(v1, nil)
-        setmetatable(v2, nil)
-        assert(v1 == v2)
+        assert(vec3.equals(v1, v2) and v1 == v2)
     end)
 
     test_lab:test('compare two vec3 with the same value', function()
         local v1 = vec3.new(1, 0, 0)
         local v2 = vec3.new(1, 0, 0)
-        assert(v1 == v2)
+        assert(vec3.equals(v1, v2))
     end)
 
     test_lab:test('compare two vec3 with different values', function()
         local v1 = vec3.new(1, 0, 0)
         local v2 = vec3.new(0, 0, 1)
-        assert(v1 ~= v2)
+        assert(not vec3.equals(v1, v2))
     end)
 end)
 
-test_lab:group('vec3.__add(v1 [table], v2 [table]) -> [vec3<v1[1] + v2[1], v1[2] + v2[2], v1[3] + v2[3]>]', function()
-    test_lab:test('same vec3', function()
+test_lab:group('vec3.add(v1 [table], v2 [table]) -> [vec3]', function()
+    test_lab:test('vec3.add(v1, v2)', function()
         local v1 = vec3.new(1, -1, 1)
-        local actual = v1 + v1
-        assert(vec3.new(2, -2, 2) == actual)
+		local v2 = vec3.new(0, 1, 0)
+        local actual = vec3.add(v1, v2)
+        assert(vec3.new(1, 0, 1):equals(actual))
     end)
 
-    test_lab:test('different vec3', function()
+    test_lab:test('v1:add(v2)', function()
         local v1 = vec3.new(1, 0, 1)
         local v2 = vec3.new(0, 2, -1)
-        local actual = v1 + v2
-        assert(vec3.new(1, 2, 0) == actual)
+        local actual = v1:add(v2)
+        assert(vec3.new(1, 2, 0):equals(actual))
     end)
 end)
 
-test_lab:group('vec3.__sub(v1 [table], v2 [table] -> [vec3<v1[1] - v2[1], v1[2] - v2[2], v1[3] - v2[3]>]', function()
-    test_lab:test('same vec3', function()
+test_lab:group('vec3.sub(v1 [table], v2 [table] -> [vec3]', function()
+    test_lab:test('vec3.sub(v1, v2)', function()
         local v1 = vec3.new(1, 0, 1)
-        local actual = v1 - v1
-        assert(vec3.zero() == actual)
+        local actual = vec3.sub(v1, v1)
+        assert(vec3.zero():equals(actual))
     end)
 
-    test_lab:test('different vec3', function()
+    test_lab:test('v1:sub(v2)', function()
         local v1 = vec3.new(0, 0, 1)
         local v2 = vec3.new(0, 1, 0)
-        local actual = v1 - v2
-        assert(vec3.new(0, -1, 1) == actual)
+        local actual = v1:sub(v2)
+        assert(vec3.new(0, -1, 1):equals(actual))
     end)
 end)
 
-test_lab:group('vec3.__mul(l [number, vec3], r [vec3, number]) -> [vec3]', function()
-
-    test_lab:test('0 * v = zero()', function()
-        local v1 = vec3.new(1, 1, 1)
-        local actual = v1 * 0
-        assert(vec3.zero() == actual)
+test_lab:group('vec3.tostring(v [vec3]) -> [string]', function()
+    test_lab:test('vec3.tostring(zero()) = <0, 0, 0>', function()
+        assert(vec3.tostring(vec3.new(0, 0, 0)) == '<0, 0, 0>')
     end)
 
-    test_lab:test('1 * v = v', function()
-        local v1 = vec3.new(1, 2, 1)
-        local actual = 1 * v1
-        assert(v1 == actual)
-        -- remove the vec3 metatable to compare that the tables are indeed different instances of vec3
-        setmetatable(v1, nil)
-        setmetatable(actual, nil)
-        assert(v1 ~= actual)
-    end)
-
-    test_lab:test('-1.5 * v', function()
-        local v1 = vec3.new(1, 0, -1)
-        local actual =  -1.5 * v1
-        assert(vec3.new(-1.5, 0, 1.5) == actual)
-    end)
-
-	test_lab:test('v * -1.5', function()
-        local v1 = vec3.new(1, 0, -1)
-        local actual =  v1 * -1.5
-        assert(vec3.new(-1.5, 0, 1.5) == actual)
+    test_lab:test('vec3.new(1, 2, -6):tostring() = <1, 2, -6>', function()
+        assert(vec3.new(1, 2, -6):tostring() == '<1, 2, -6>')
     end)
 end)
 
---[[
-test_lab:group('vec3.__div(v [vec3], k [non-zero number]) -> [vec3]', function()
-    test_lab:test('v / 0 = error(divide by zero)', function()
-        local v1 = vec3.new(1, 0, 0)
-        local result, message = pcall(function() local v2 = v1 / 0 end)
-        assert(result == false)
-        assert(string.find(message, 'error: divide by zero'))
-    end)
-
-    test_lab:test('v / 1 = v', function()
-        local v1 = vec3.new(2, 1, 3)
-        local actual = v1 / 1
-        assert(v1 == actual)
-        -- remove the vec3 metatable to compare that the tables are indeed different instances of vec3
-        setmetatable(v1, nil)
-        setmetatable(actual, nil)
-        assert(v1 ~= actual)
-    end)
-
-    test_lab:test('v / 2', function()
-        local v1 = vec3.new(1, 2, -4)
-        local actual = v1 / 2
-        assert(vec3.new(0.5, 1, -2) == actual)
-    end)
-end)
---]]
-
-test_lab:group('vec3.__tostring(v [vec3]) -> [string "<v[1], v[2], v[3]>"]', function()
-    test_lab:test('tostring(zero()) = <0, 0, 0>', function()
-        assert(tostring(vec3.new(0, 0, 0)) == '<0, 0, 0>')
-    end)
-
-    test_lab:test('tostring(<1, 2, -6>) = <1, 2, -6>', function()
-        assert(tostring(vec3.new(1, 2, -6)) == '<1, 2, -6>')
-    end)
-end)
-
---[[
--- vec3 self instance modification tests 
---]]
 test_lab:group('vec3:set(x [number], y [number], z [number]) ->', function()
     test_lab:test('change current x,y,z values', function()
         local v1 = vec3.new(-1, 2, 0)
         v1:set(0, 1, 0)
-        assert(v1[1] == 0 and v1[2] == 1, v1[3] == 0)
+        assert(v1.x == 0 and v1.y == 1, v1.z == 0)
     end)
 end)
 
-test_lab:group('vec3:_add(v [table]) -> [self<self[1] + v[1], self[2] + v[2], self[3] + v[3]>]', function()
+test_lab:group('vec3:_add(v [table]) ->', function()
     test_lab:test('add zero() to current vec3', function()
         local v1 = vec3.new(1, 2, 4)
         local v2 = vec3.zero()
         local actual = v1:_add(v2)
-        assert(actual == v1)
-
-        -- remove the vec3 metatable to compare that the tables are indeed the same instance of vec3
-        setmetatable(v1, nil)
-        setmetatable(actual, nil)
-        assert(v1 == actual)
+		assert(actual == v1) -- same table
+        assert(v1.x == 1 and v1.y == 2 and v1.z == 4)
     end)
 
     test_lab:test('add non-zero vec3 to current vec3', function()
         local v1 = vec3.new(1, 2, 4)
         local v2 = vec3.new(-1, 1, -5)
         local actual = v1:_add(v2)
-        local expected = vec3.new(0, 3, -1)
-        assert(expected == v1)
-        assert(expected == actual)
-
-        -- remove the vec3 metatable to compare that the tables are indeed the same instance of vec3
-        setmetatable(v1, nil)
-        setmetatable(actual, nil)
-        assert(v1 == actual)
+		assert(actual == v1) -- same table
+		assert(v1.x == 0 and v1.y == 3 and v1.z == -1)
     end)
 end)
 
-test_lab:group('vec3:_sub(v [table]) -> [self<self[1] - v[1], self[2] - v[2], self[3] - v[3]>]', function()
+test_lab:group('vec3:_sub(v [table]) -> [self<self.x - v.x, self.y - v.y, self.z - v.z>]', function()
     test_lab:test('subtract vec3 from current vec3', function()
         local v1 = vec3.new(1, -1, 2)
         local v2 = vec3.new(0, 2, 1)
         local actual = v1:_sub(v2)
-        local expected = vec3.new(1, -3, 1)
-        assert(expected == v1)
-        assert(expected == actual)
-
-        -- remove the vec3 metatable to compare that the tables are indeed the same instance of vec3
-        setmetatable(v1, nil)
-        setmetatable(actual, nil)
-        assert(v1 == actual)
+		assert(actual == v1) -- same table
+		assert(v1.x == 1 and v1.y == -3 and v1.z == 1)
     end)
 end)
 
-test_lab:group('vec3:_scale(k [number]) -> [self<v[1]*k, v[2]*k, v[3]*k>]', function()
+test_lab:group('vec3:_scale(k [number]) -> [self<v.x*k, v.y*k, v.z*k>]', function()
     test_lab:test('apply a scalar to the current vec3', function()
         local v1 = vec3.new(1.5, -2, 1)
         local actual = v1:_scale(-2)
-        local expected = vec3.new(-3, 4, -2)
-        assert(expected == v1)
-        assert(expected == actual)
-
-        -- remove the vec3 metatable to compare that the tables are indeed the same instance of vec3
-        setmetatable(v1, nil)
-        setmetatable(actual, nil)
-        assert(v1 == actual)
+		assert(actual == v1)
+		assert(v1.x == -3 and v1.y == 4 and v1.z == -2)
     end)
 end)
-
---[[
-test_lab:group('vec3:div(k [non-zero number]) -> [self<v[1]/k, v[2]/k, v[3]/k>]', function()
-    test_lab:test('divide current vec3 by a scalar', function()
-        local v1 = vec3.new(2, -3, 1)
-        local actual = v1:div(2)
-        local expected = vec3.new(1, -1.5, 0.5)
-        assert(expected == v1)
-        assert(expected == actual)
-
-        -- remove the vec3 metatable to compare that the tables are indeed the same instance of vec3
-        setmetatable(v1, nil)
-        setmetatable(actual, nil)
-        assert(v1 == actual)
-    end)
-end)
---]]
