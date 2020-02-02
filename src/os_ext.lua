66af3b585f06e5d9008b9e653d03eb747567b0d6
intxparts <jcowens.dx@gmail.com>
2020-02-02 13:37:39 -0500

    convert: spaces -> tabs

--- src/os_ext.lua
+++ src/os_ext.lua
@@ -1,15 +1,15 @@
 local os_ext = {}
 
 os_ext.platforms = { 
-    unix = 'unix', 
-    windows = 'windows' 
+	unix = 'unix', 
+	windows = 'windows' 
 }
 
 os_ext.path_separator = package.config:sub(1, 1)
 
 local _separator_platform_table = { 
-    ['/'] = os_ext.platforms.unix, 
-    ['\\'] = os_ext.platforms.windows 
+	['/'] = os_ext.platforms.unix, 
+	['\\'] = os_ext.platforms.windows 
 }
 
 os_ext.platform = _separator_platform_table[os_ext.path_separator]
@@ -18,16 +18,16 @@ os_ext.is_unix = os_ext.platform == os_ext.platforms.unix
 
 -- Returns a table of results from a command string being invoked
 function os_ext.run_command(command)
-    assert(type(command) == 'string')
-    local idx = 1
-    local results = {}
-    local pfile = io.popen(command)
-    for line in pfile:lines() do
-        results[idx] = line
-        idx = idx + 1
-    end
-    pfile:close()
-    return results
+	assert(type(command) == 'string')
+	local idx = 1
+	local results = {}
+	local pfile = io.popen(command)
+	for line in pfile:lines() do
+		results[idx] = line
+		idx = idx + 1
+	end
+	pfile:close()
+	return results
 end
 
 return os_ext
